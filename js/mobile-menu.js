0b0647e404c3d9da1d97126df8cd2f7dda81752a
YanaHolenok <111532651+YanaHolenok@users.noreply.github.com>
2022-11-11 11:42:06 +0200

    add-2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,52 +1,15 @@
+
 (() => {
-    const mobileMenu = document.querySelector('.mobile-menu');
-    const desktopMenu = document.querySelector('.desktop-menu');
-    const openMenuBtn = document.querySelector('.mobile-btn--open');
-    const closeMenuBtn = document.querySelector('.mobile-btn--close');
-    const bodyScroll = document.querySelector('.body');
-    const sectionWorks = document.querySelector('.works');
-
-  
-    const toggleMenu = () => {
-      const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-      mobileMenu.classList.toggle('is-hidden');
-      openMenuBtn.classList.toggle('is-hidden');
-      bodyScroll.classList.toggle('scroll');
-
-
-      const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-  };
-  
-    openMenuBtn.addEventListener('click', toggleMenu);
-    closeMenuBtn.addEventListener('click', toggleMenu);
-    
-  
-    // Close the mobile menu on wider screens if the device orientation changes
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-      if (!e.matches) return;
-      
-      desktopMenu.classList.remove('visually-hidden');
-      openMenuBtn.setAttribute('aria-expanded', false);
-      bodyScrollLock.enableBodyScroll(document.body);
-    });
-    
-    window.matchMedia('(max-width: 768px)').addEventListener('change', e => {
-      if (!e.matches) return;
-          desktopMenu.classList.add('visually-hidden');
-    });
-
-    window.matchMedia('(max-width: 1199px)').addEventListener('change', e => {
-      if (!e.matches) return;
-      sectionWorks.classList.add('visually-hidden');
-    });
-    window.matchMedia('(min-width: 1200px)').addEventListener('change', e => {
-      if (!e.matches) return;
-      sectionWorks.classList.remove('visually-hidden');
-    });
-  
-  })();
+    const refs = {
+        openMenuBtn: document.querySelector("[data-menu-open]"),
+        closeMenuBtn: document.querySelector("[data-menu-close]"),
+        menu: document.querySelector("[data-menu]"),
+        };
+        
+        refs.openMenuBtn.addEventListener("click", toggleMenu);
+        refs.closeMenuBtn.addEventListener("click", toggleMenu);
+        
+        function toggleMenu() {
+        refs.menu.classList.toggle("is-hidden");
+        }
+})();
